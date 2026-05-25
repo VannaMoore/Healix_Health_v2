13e09a09b2e80ab4455bd442829ea340e842ff07
VannaMoore <vannamoore@gmail.com>
2026-05-25 18:15:42 -0500

    feat: add ProviderSpecialty entity class

---
+++ HealixHealth/HealixAPI/Models/ProviderSpecialty.cs
@@ -0,0 +1,12 @@
+﻿// Purely a junction table
+namespace HealixAPI.Models
+{
+    public class ProviderSpecialty
+    {
+        public Guid ProviderId { get; set; } // composite key
+        public Provider Provider { get; set; } = null!; // navigational propety
+        public int SpecialtyId { get; set; } // composit key
+        public Specialty Specialty { get; set; } = null!; // navigational property
+
+    }
+}
